31bbb82d65e456944d91ee8cd436a4c8ee3b2add
Cameron <camjsykes@gmail.com>
2025-03-15 17:28:54 -0400

    Quad-1: Add locks and spawn processes to control parallelized processes

--- Sensors/Quad.py
+++ Sensors/Quad.py
@@ -5,29 +5,45 @@ from LightSensor.LightSensor import LightSensor
 import dht11
 import RPi.GPIO as GPIO
 import time
+import Globals
+from multiprocessing import Process, Lock
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-distanceSensor = RangeSensor(12, 10, "Range Sensor")
-lightSensor = LightSensor(8, "Light Sensor")
-# tempHumiditySensor = dht11.DHT11(pin = 10)
-print("")
+SensorLocks = [Lock() for l in range(NUM_LOCKS)]
 
+# Instantiate sensors
+LightSensors = [LightSensor(SensorLocks[0], 0,  8,     "Light Sensor 0")]
+RangeSensors = [RangeSensor(SensorLocks[1], 1, 12, 10, "Range Sensor 0")]
+
+# Acquire locks to confirm instantiation
+for lock in SensorLocks:
+    lock.acquire()
+    lock.release()
+
+# Spawn sensor processes
+for light_sensor in LightSensors:
+    Process(target=light_sensor.LightSensor_AppMain).start()
+
+for range_sensor in RangeSensor:
+    Process(target=range_sensor.RangeSensor_AppMain).start()
+
+# Continually poll sensors
 try:
     while True:
-        if distanceSensor.isClose():
-            # Take corrective action
-            print("TOO CLOSE")
+        # Attempt to acquire locks from each sensor as
+        # a signal that the sensor is done with its operation
+        for lock in SensorLocks:
+            lock.acquire()
+            print("Lock acquired")
 
-        lightSensor.chargeTiming()
-        
-#       result = tempHumiditySensor.read()
-#       if result.is_valid():
-#           print(f"Temperature and Humidity Sensor - Temperature:\t{result.temperature:-3.1f} C")
-#           print(f"Temperature and Humidity Sensor - Humidity:\t{result.humidity:-3.1f}%")
-        
-        time.sleep(.5)
-        print("")
+            # Release the lock so that the sensor can do its
+            # next operation
+            lock.release()
+            print("Lock released")
 except KeyboardInterrupt:
+    for sensor in range(NUM_SENSORS):
+        AppRunStates[sensor] = EXIT
+
     GPIO.cleanup()
