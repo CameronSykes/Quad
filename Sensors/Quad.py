282c13284729799f21650a09bcb4d7e22b228277
Cameron <camjsykes@gmail.com>
2025-03-15 16:20:18 -0400

    Central function that runs all child sensors

---
+++ Sensors/Quad.py
@@ -0,0 +1,33 @@
+#! /bin/python3
+
+from RangeSensor.RangeSensor import RangeSensor
+from LightSensor.LightSensor import LightSensor
+import dht11
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+distanceSensor = RangeSensor(12, 10, "Range Sensor")
+lightSensor = LightSensor(8, "Light Sensor")
+# tempHumiditySensor = dht11.DHT11(pin = 10)
+print("")
+
+try:
+    while True:
+        if distanceSensor.isClose():
+            # Take corrective action
+            print("TOO CLOSE")
+
+        lightSensor.chargeTiming()
+        
+#       result = tempHumiditySensor.read()
+#       if result.is_valid():
+#           print(f"Temperature and Humidity Sensor - Temperature:\t{result.temperature:-3.1f} C")
+#           print(f"Temperature and Humidity Sensor - Humidity:\t{result.humidity:-3.1f}%")
+        
+        time.sleep(.5)
+        print("")
+except KeyboardInterrupt:
+    GPIO.cleanup()
