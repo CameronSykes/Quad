ee7b0295147071b70a031eb18a5b4777c038acfb
Cameron <camjsykes@gmail.com>
2025-03-27 21:30:35 -0400

    Quad-29: Instantiate a shift register object and remove superfluous stuff

--- ShiftRegisters/run.py
+++ ShiftRegisters/run.py
@@ -3,47 +3,24 @@
 import RPi.GPIO as GPIO
 import time
 import sys
-from queue import Queue
-
-low        = False
-high       = True
-A          = -1
-SHIFT_CLK  = -1
-RESET      = -1
-LATCH_CLK  = -1
-OUT_ENA    = -1
-InputQueue = Queue(maxsize=8)
+from SR_74HC595.SR_74HC595 import SR_74HC595
 
 if __name__ == "__main__":
-    A             = 23 # GPIO
-    SHIFT_CLK     = 24 # GPIO
-    RESET         = 25 # GPIO
-    LATCH_CLK     = 8  # GPIO
-    OUT_ENA       = 7  # GPIO
-
-    PIN_NUMBERING = GPIO.BCM
-
-    name          = f'(74HC595) - {input("Name: ")}'
-    LoopCount     = 0
+    # Instantiate shift register
+    ShiftReg1 = SR_74HC595(name="Shift Register 1", a=23, shift_clk=24, reset=25, latch_clk=8, output_ena=7, use_board_nums=False)
+    ShiftReg1.output()
 
     try:
         if len(sys.argv) == 1:
             # Interactive input
             while True:
-                if LoopCount % 8 == 0 and LoopCount != 0:
-                    output()
-                else:
-                    value = int(input("Input: "))
-                    shift(value)
-
-                LoopCount += 1
+                value = int(input("Input: "))
+                ShiftReg1.shift(value)
         else:
+            pass
             # Command line input
-            for Position, WriteVal in enumerate(sys.argv[1]):
-                shift(WriteVal)
-
-                if Position % 7 == 0 and Position != 0:
-                    output()
+            for WriteVal in sys.argv[1]:
+                ShiftReg1.shift(WriteVal)
     except KeyboardInterrupt:
         print()
         print('Exiting')
